ca95b70948df27f46a4ece16fd8c6aa413e162cf
Николай Шарапов <nsharapov@avanpost.ru>
2022-09-26 12:42:16 +0300

    Add pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/Moriartii/translator/pkg/starter"
+)
+
 func main() {
-// TODO
+	starter.Run()
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Moriartii/translator
+
+go 1.19
